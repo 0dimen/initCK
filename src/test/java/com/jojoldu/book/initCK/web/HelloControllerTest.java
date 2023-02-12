bda0852ee68bee703aa817f963426a83e16226d4
0dimen <first11.konkuk.ac.kr>
2023-02-12 14:58:32 +0900

    8.4 스프링 부트 프로젝트로 RDS 접근하기

--- src/test/java/com/jojoldu/book/initCK/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/initCK/web/HelloControllerTest.java
@@ -10,15 +10,10 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 
@@ -52,9 +47,5 @@ public class HelloControllerTest {
                         .param("name", name)
                         .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk());
-                //.andExpect(jsonPath("$.name", is(name)))
-                //.andExpect(jsonPath("$.amount", is(amount)));
     }
-
-
 }
